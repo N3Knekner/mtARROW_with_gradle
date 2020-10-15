a60e5bacfe75e07a435926495fff1e31d2704550
EvandroSocrepa <evandrosocrepa2@gmail.com>
2020-10-15 13:10:12 -0300

    Fixed: Error logj42 and connection to server.

--- src/main/java/org/screenplay/localhostClient.java
+++ src/main/java/org/screenplay/localhostClient.java
@@ -1,11 +1,7 @@
 package org.screenplay;
 
-import com.fazecast.jSerialComm.SerialPort;
-
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -13,48 +9,63 @@ import java.net.InetAddress;
 public class localhostClient {
 
     public void wifi(){
-        System.out.println("Conectando..");
-        String host_Ip = "10.0.1.104";
+        System.out.flush();
+        System.out.println("start");
+        InetAddress address;
+        DatagramSocket socket;
+        DatagramPacket response;
+        Robot mechanics;
+
+        String host_Ip = "10.0.1.102";
         int port = 4821;
-        byte[] buffer = new byte[512];
+        byte[] buffer = new byte[14];
 
         try {
-            InetAddress address = InetAddress.getByName(host_Ip);
-            DatagramSocket socket = new DatagramSocket();
-            DatagramPacket request = new DatagramPacket(new byte[1], 1, address, port);
-            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-            socket.send(request);
+            mechanics = new Robot();
+            address = InetAddress.getByName(host_Ip);
+            socket = new DatagramSocket();
+            response = new DatagramPacket(buffer, buffer.length);
 
-            Robot mechanics = new Robot();
-
-            /*if(!socket.isConnected()){
-                System.out.println("Nao conectado");
-                return;
-            }*/
-
-            new Thread() { //thread padrao da versao q eu tenho
+            new Thread() {
                 @Override
                 public void run() {
+                    int x = 0;
+                    String command[];
+
+                    while(true) {
+                        try {
+
+                            System.out.println("1");
+                            sleep(1000);
+                            socket.send(new DatagramPacket(new byte[1], 1, address, 4821));
+                            socket.receive(response);
+                            System.out.println("2");
+
+                       /*if (socket.getInetAddress() == null) {
+                           run();
+                       }*/
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            break;
+                        }
+                    }
+                   
+
                     for (; ; ) {
                         try {
-                            String command[] = new String[4];
-
+                            System.out.println(socket);
                             socket.receive(response);
-                            command = (new String(buffer, 0, response.getLength())).split(",");
+                            command = (new String(buffer, 0, response.getLength() - 2)).split(",");
+                            System.out.println(command[0] + "," + command[1]);
+                            mechanics.mouseMove(Integer.parseInt(command[0]), Integer.parseInt(command[1]));
 
-                            mechanics.mouseMove(Integer.parseInt(command[0]), Integer.parseInt(command[1])); //move o marze
-
-                        } catch (Exception e){
-                            System.out.println("disconectado");
-                            return;
+                        } catch (NumberFormatException | IOException e) {
                         }
                     }
                 }
             }.start();
 
-        } catch (Exception e) {
-            System.out.println(e);
-            return;
+        } catch (Exception e){
         }
     }
 }
